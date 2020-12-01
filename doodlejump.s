03d23460bd08923094c5f378891d11c4c3ecd3b6
Ammar <ammart500@gmail.com>
2020-12-01 05:05:26 -0500

    scrolling done

--- doodlejump.s
+++ doodlejump.s
@@ -38,6 +38,11 @@
 .text
 
 main:
+	lw $t0, displayAddress	
+	la $t1, colors	
+	
+	jal clearScreen
+	
 	# Wait for signal (start, restart, exit)
 	lw $t8, 0xffff0000 
 	beq $t8, 1, initGame
@@ -45,43 +50,37 @@ main:
 
 	initGame:
 		lw $t5, 0xffff0004
-		beq $t5, 0x71, Exit # Press Q
+		beq $t5, 0x71, Exit 	 # Press Q
 		beq $t5, 0x72, StartLoop # Press R
 		beq $t5, 0x73, StartLoop # Press T
 		j main
 
 	StartLoop: 
-		lw $t0, displayAddress	
-		la $t1, colors	
-		
-		jal clearScreen
-		
 		li $a0, 675
 		li $a1, 250
 		jal createLevel
 		
-		addi $t3, $t3, 4
 		li $a0, 75
 		li $a1, 200
 		jal createLevel
-		
-		addi $t3, $t3, 4
+	
 		li $a0, 325
 		li $a1, 150
 		jal createLevel
 
-		addi $t3, $t3, 4
 		li $a0, 530
 		li $a1, 150
 		jal createLevel
 		
 		lw $t2, 0($t1)
 		sw $t2, 0($sp)
+		
 		# Jump height limit counter (limit = 10)
 		li $t9, 0
+		li $t3, 0
 		
 		# Game loop
-		RUN: 
+		RUN: 			
 			ble $t9, 10, flyUp
 			flyDown:
 				jal RepaintFlyDown
@@ -101,9 +100,53 @@ main:
 				# Handle left and right movement
 				lw $t8, 0xffff0000 
 				beq $t8, 1, leftOrRight
-			
+				
+				# Scroll screen up
+				ble $t0, 268465160, Scroll
+				
 				j RUN
 
+Scroll:
+	li $t6, 0
+	lw $t3, displayAddress
+	
+	# loop each pixel
+	loopPixel:
+		# get color of block
+		lw $t8, 0($t3)
+		# if green then lower it
+		li $t4, 0
+		beq $t8, -11946474, scrollLevel
+	
+		nextPixel: 
+			addi $t3, $t3, 4
+			addi $t6, $t6, 1
+			blt $t6, 1024, loopPixel
+			
+			li $a0, 1
+			li $a1, 20
+			jal createLevel
+		
+			j RUN
+	
+	scrollLevel:
+		# load blue
+		lw $t2, 0($t1)
+		# color current blue
+		sw $t2, 0($t3)
+		# load green color
+		lw $t2, 8($t1)
+		# color bottom block green
+		sw $t2, 128($t3)
+		
+		# increment counter and address
+		addi $t3, $t3, 4
+		addi $t4, $t4, 1
+		blt $t4, 6, scrollLevel
+		
+		addi $t3, $t3, 128
+		j nextPixel
+	
 RepaintFlyUp:
 	# push above-block colors onto stack
 	addi $sp, $sp, -8
@@ -148,7 +191,7 @@ createLevel:
 	# copy lowerbound of level location range
 	move $t4, $a0
 	# copy display address
-	move $t5, $t0
+	lw $t5, displayAddress
 	
 	# generate random number
 	li $v0, 42
@@ -157,7 +200,7 @@ createLevel:
 	add $a0, $a0, $t4
 	move $t6, $a0
 	
-	# make multiple of 4
+	# make multiple of 4 (x * 2^2)
 	sll $t6, $t6, 2
 	add $t5, $t5, $t6	
    	
@@ -180,13 +223,11 @@ hitLevel:
 	lw $t3, 4164($t0)
 	# green = -11946474
 	beq $t3, -11946474, resetFly
-	
 	jr $ra
 	
-	resetFly:
+	resetFly:			
 		li $t9, 0
-	
-	jr $ra
+		jr $ra
 		
 clearScreen:
 	li $t6, 0
@@ -194,12 +235,15 @@ clearScreen:
 	NotCleared:
 		sw $t2, 0($t0)
 		addi $t0, $t0, 4
-		
 		addi $t6, $t6, 1
+		li $v0, 32
+		li $a0, 3
+		syscall
 		blt $t6, 1024, NotCleared
-		lw $t0, displayAddress
-		li $t6, 0
-		jr $ra
+		
+	lw $t0, displayAddress
+	li $t6, 0
+	jr $ra
 		
 leftOrRight:
 	lw $t5, 0xffff0004 
@@ -218,3 +262,12 @@ moveRight:
 Exit:
 	li $v0, 10 
 	syscall
+	
+	
+		#li $v0, 1
+		#move $a0, $t6
+		#syscall
+		# Display newline
+		#li $v0, 4
+		#la $a0, newline
+		#syscall 
